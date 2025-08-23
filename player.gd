2c99d97d5af64d139396497032ead96f62b9bd27
Apinoita <131701872+Apinoita@users.noreply.github.com>
2025-08-23 19:11:37 +0300

    added prtojectiles and finished movement and mad camera move

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -900.0
+@onready var main = get_tree().get_root().get_node("Node")
+@onready var projectile = load("res://projectile.tscn")
 
 
 func _physics_process(delta: float) -> void:
@@ -11,14 +13,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	
+		
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_just_pressed("Shoot"):
+			var instance = projectile.instantiate()
+			instance.spawnpos = global_position
+			main.add_child.call_deferred(instance)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
