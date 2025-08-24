ccb8b4cdc84013bcbaedb8be4d01ff46591eeb36
Apinoita <131701872+Apinoita@users.noreply.github.com>
2025-08-24 09:59:08 +0300

    projectiles work now propperly

--- player.gd
+++ player.gd
@@ -3,9 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
+var hitboxsize = 125
 @onready var main = get_tree().get_root().get_node("Node")
 @onready var projectile = load("res://projectile.tscn")
 
+func shoot(mouseloc) -> void:
+	if Input.is_action_just_pressed("Shoot"):
+		var viewportsize = get_viewport().get_visible_rect().size
+		var dir
+		if (-mouseloc.y+viewportsize.y/2) >= 0:
+			dir = atan((-mouseloc.y+viewportsize.y/2)/(mouseloc.x-viewportsize.x/2))
+			if (mouseloc.x-viewportsize.x/2) < 0:
+				dir = PI+dir
+		else:
+			dir = atan((-mouseloc.y+viewportsize.y/2)/(mouseloc.x-viewportsize.x/2))
+			if (mouseloc.x-viewportsize.x/2) < 0:
+				dir = -PI+dir
+		print(dir)
+		var instance = projectile.instantiate()
+		instance.spawnpos = Vector2(global_position.x+cos(dir)*hitboxsize, global_position.y-sin(dir)*hitboxsize)
+		instance.spawndir = dir
+		main.add_child.call_deferred(instance)
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		shoot(event.position)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,10 +48,6 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
-		if Input.is_action_just_pressed("Shoot"):
-			var instance = projectile.instantiate()
-			instance.spawnpos = global_position
-			main.add_child.call_deferred(instance)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
--- projectile.gd
+++ projectile.gd
@@ -1,28 +1,14 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 const SPEED = 3000.0
 const JUMP_VELOCITY = -400.0
 
 var spawnpos
+var spawndir
 
 func _ready() -> void:
 	
 	var direction := Input.get_axis("Left", "Right")
-	global_position = Vector2(spawnpos.x+(direction*100),spawnpos.y)
-	velocity.x = direction * SPEED
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta * 1/2
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	
-# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	move_and_slide()
+	global_position = spawnpos
+	apply_central_force(Vector2(cos(spawndir)*SPEED, -sin(spawndir)*SPEED))
